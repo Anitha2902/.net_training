f828a101cb59b5b99256c73fb110bdf2da564b9f
Anitha Gayatri <anithaga@infinite.com>
2024-07-10 11:23:25 +0530

    cc4

---
+++ Csharp/assessment/cc4/cc4/Append.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Append
+{
+    static void Main()
+    {
+        string filePath = "sample.txt";
+        string textToAdd = "This text will be appended to the file.";
+
+       
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine(textToAdd);
+        }
+
+        Console.WriteLine("Text has been appended to the file.");
+        Console.ReadLine();
+    }
+}
---
+++ Csharp/assessment/cc4/cc4/Delegates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public delegate int CalculatorDelegate(int num1, int num2);
+
+class Delegate
+{
+    static void Main()
+    {
+       
+        CalculatorDelegate addition = (a, b) => a + b;
+        CalculatorDelegate subtraction = (a, b) => a - b;
+        CalculatorDelegate multiplication = (a, b) => a * b;
+
+        
+        Console.Write("Enter the first number: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+       
+        int resultAddition = addition(number1, number2);
+        int resultSubtraction = subtraction(number1, number2);
+        int resultMultiplication = multiplication(number1, number2);
+
+        
+        Console.WriteLine($"Result of Addition: {resultAddition}");
+        Console.WriteLine($"Result of Subtraction: {resultSubtraction}");
+        Console.WriteLine($"Result of Multiplication: {resultMultiplication}");
+
+        Console.ReadLine();
+    }
+}
