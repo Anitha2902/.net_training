0dc264d2f78aa35d3d7d5cbeffb7199255a2f8e6
Anitha Gayatri <anithaga@infinite.com>
2024-08-12 10:29:48 +0530

    Assignment1

---
+++ ASP.net/Assignments/Assignment1/Assignment1/Products.aspx.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment1
+{
+    public partial class Products : System.Web.UI.Page
+    {
+        Dictionary<string, (string ImageUrl, decimal Price)> products = new Dictionary<string, (string, decimal)>
+        {
+            {"Toys",("~/images/Toys.jfif", 200m) },
+            {"Perfumes",("~/images/Perfumes.jfif", 1000m) },
+            {"Jewellery",("~/images/Jewellery.jfif", 50000m) }
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                foreach (var product in products.Keys)
+                {
+                    ddlProducts.Items.Add(product);
+                }
+            }
+
+        }
+        protected void ddlProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedProduct = ddlProducts.SelectedItem.Text;
+            if (products.ContainsKey(selectedProduct))
+            {
+                imgProduct.ImageUrl = products[selectedProduct].ImageUrl;
+            }
+        }
+        protected void btnGetPrice_Click(object sender, EventArgs e)
+        {
+            string selectedProduct = ddlProducts.SelectedItem.Text;
+            if (products.ContainsKey(selectedProduct))
+            {
+                lblPrice.Text = "Price $" + products[selectedProduct].Price.ToString();
+            }
+        }
+    }
+}
